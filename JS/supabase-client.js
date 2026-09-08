49b51aa2c56097dca8099f527ee2f31e21e69301
LuisUGTO <la.duranalvarez@ugto.mx>
2026-09-07 19:15:06 -0600

    Update supabase-client.js

--- JS/supabase-client.js
+++ JS/supabase-client.js
@@ -1,6 +1,5 @@
 // JS/supabase-client.js
 const supabaseUrl = 'https://pcrukixlooulranpzpsm.supabase.co';
-const supabaseKey = 'sb_publishable_rpAqa2vKCUrtyg3Ay1aXvg_Xf6Ja_qp';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjcnVraXhsb291bHJhbnB6cHNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4MDA4NjcsImV4cCI6MjEwNDM3Njg2N30.Xf_J2_jPcx8h7XHyGoKLBIbcbIoS5FKku1ycuKs-J74';
 
-// Usamos 'db' para evitar cualquier conflicto de nombres
 const db = window.supabase.createClient(supabaseUrl, supabaseKey);
