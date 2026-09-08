f75df6c2e07d37b3fddd1ebebe1fa8b267391e75
LuisUGTO <la.duranalvarez@ugto.mx>
2026-09-07 18:44:44 -0600

    Update supabase-client.js

--- JS/supabase-client.js
+++ JS/supabase-client.js
@@ -1,9 +1,7 @@
-// js/supabase-client.js
+// JS/supabase-client.js
 
-// 1. URL del proyecto y Llave Pública (Anon/Publishable key)
 const supabaseUrl = 'https://pcrukixlooulranpzpsm.supabase.co';
 const supabaseKey = 'sb_publishable_rpAqa2vKCUrtyg3Ay1aXvg_Xf6Ja_qp';
 
-// 2. Inicializamos la conexión
+// Inicializamos la conexión global una sola vez
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-console.log("Conexión a Supabase inicializada correctamente.");
